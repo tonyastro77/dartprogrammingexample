413a25388e523093c7c884dff15c04945c3f4287
tonyastro77 <tonyastro77@users.noreply.github.com>
2019-11-04 16:39:47 +0200

    first app

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+      questionIndex = questionIndex + 1;
+      print(questionIndex);
+    }
+
+  @override
+  Widget build(BuildContext context) {    
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Mi primera aplicación'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'), 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2'),
+              ),
+            RaisedButton(
+              child: Text('Answer 3'), 
+              onPressed: answerQuestion,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
